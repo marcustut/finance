a090f366995db5f9894010fa721c5a2d85323c5c
Marcus Lee <marcustutorial@hotmail.com>
2022-06-13 08:38:26 +0800

    added notion api

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,18 +1,40 @@
 extern crate dotenv;
 
 use dotenv::dotenv;
-use std::error::Error;
+use notion::ids::DatabaseId;
+use notion::NotionApi;
+use serde::Deserialize;
+use std::{error::Error, str::FromStr};
 use teloxide::{prelude::*, utils::command::BotCommands};
 
 #[tokio::main]
 async fn main() {
     // load environment variables from .env
-    dotenv().ok();
+    dotenv().expect("Failed to read .env file");
 
+    // parse environment variables into config struct
+    let bot_config = envy::from_env::<BotConfig>().expect("Couldn't read bot config");
+
+    // initialize logger
     pretty_env_logger::init();
     log::info!("Starting throw dice bot...");
 
-    let bot = Bot::from_env().auto_send();
+    // create a Bot instance from token
+    let bot = Bot::new(bot_config.telegram_bot_token);
+
+    // create an instace of Notion API client
+    let notion_api = NotionApi::new(bot_config.notion_api_token)
+        .expect("Couldn't connect to Notion API with the provided token");
+
+    let db = notion_api
+        .get_database(
+            DatabaseId::from_str(&*bot_config.finance_database_id)
+                .expect("unable to parse database_id"),
+        )
+        .await
+        .expect("Failed to get the database");
+
+    println!("{:?}", db);
 
     // teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
     //     bot.send_dice(message.chat.id).await?;
@@ -20,7 +42,14 @@ async fn main() {
     // })
     // .await;
 
-    teloxide::commands_repl(bot, answer, Command::ty()).await;
+    // teloxide::commands_repl(bot, answer, Command::ty()).await;
+}
+
+#[derive(Deserialize, Debug)]
+struct BotConfig {
+    telegram_bot_token: String,
+    notion_api_token: String,
+    finance_database_id: String,
 }
 
 #[derive(BotCommands, Clone)]
